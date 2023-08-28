3ef4dcd7cfac8bb08c26c276383c347515c89f7a
kondrat1206 <kondrat1206@gmail.com>
2023-08-28 11:52:19 +0300

    new funcs

---
+++ main.py
@@ -0,0 +1,13 @@
+def parser(string):
+    pass
+
+def handler():
+    pass
+
+def main():
+    while True:
+        source_command = input("Enter command: ").lower()
+        command = parser(source_command)
+
+if __name__ == "__main__":
+    main()
